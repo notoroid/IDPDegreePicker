2768eb7c3bb91045aca6d52fa4402eafd400bc61
notoroid <noto@irimasu.com>
2014-04-04 02:30:26 +0900

    supports saving size slider.

--- Lib/IDPDegreePicker.m
+++ Lib/IDPDegreePicker.m
@@ -10,6 +10,7 @@
 #import "IDPDegreePickerBaseView.h"
 #import "IDPDegreeShadowView.h"
 #import "IDPDegreeCenterView.h"
+#import "IDPDegreePickerLowBaseView.h"
 
 @interface IDPDegreePicker () <UIScrollViewDelegate>
 {
@@ -24,17 +25,20 @@
     self.backgroundColor = [UIColor colorWithRed:.08f green:.08f blue:.08f alpha:1];
     
 #define IDP_DEGREE_SLIDER_HEIGHT 144.0f
+#define IDP_LOW_DEGREE_SLIDER_HEIGHT 72.0f
+
+    const CGFloat sliderheight = self.frame.size.height > IDP_LOW_DEGREE_SLIDER_HEIGHT ?IDP_DEGREE_SLIDER_HEIGHT : IDP_LOW_DEGREE_SLIDER_HEIGHT;
     
-    _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(.0f, self.frame.size.height - IDP_DEGREE_SLIDER_HEIGHT, 320.0f, IDP_DEGREE_SLIDER_HEIGHT)];
-    _scrollView.contentSize = CGSizeMake(920, IDP_DEGREE_SLIDER_HEIGHT);
+    _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(.0f, self.frame.size.height - sliderheight, 320.0f, sliderheight)];
+    _scrollView.contentSize = CGSizeMake(920, sliderheight);
     _scrollView.bounces = NO;
     _scrollView.delegate = self;
     _scrollView.showsHorizontalScrollIndicator = NO;
     _scrollView.showsHorizontalScrollIndicator = NO;
     [self addSubview:_scrollView];
     
-    UIView* boardView = [[IDPDegreePickerBaseView alloc] init] /*[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"DegreeSliderBase.png"]]*/;
-    boardView.frame = CGRectMake(.0f, .0f, 920.0f, 144.0f);
+    UIView* boardView = sliderheight == IDP_DEGREE_SLIDER_HEIGHT ? [[IDPDegreePickerBaseView alloc] init] : [[IDPDegreePickerLowBaseView alloc] init] /*[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"DegreeSliderBase.png"]]*/;
+    boardView.frame = CGRectMake(.0f, .0f, 920.0f, sliderheight);
     boardView.opaque = NO;
     [_scrollView addSubview:boardView];
 #define IDP_DEGREE_SLIDER_CENTER_POSITION (920.0f * .5f - 160.0f)
@@ -48,13 +52,13 @@
     
     
     UIView *shadowView = [[IDPDegreeShadowView alloc] init] /*[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"DegreeSliderShadow.ong"]]*/;
-    shadowView.frame = CGRectMake(.0f, .0f, 320.0f, 144.0f);
+    shadowView.frame = CGRectMake(.0f, .0f, 320.0f, sliderheight);
     shadowView.opaque = NO;
     shadowView.userInteractionEnabled = NO;
     [self addSubview:shadowView];
     
     UIView *centerView = [[IDPDegreeCenterView alloc] init] /*[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"DegreeCenter.png"]]*/;
-    centerView.frame = CGRectMake( 160.0f - 4.0f * .5f,144.0f - 48.0f , 4.0f, 48.0f);
+    centerView.frame = CGRectMake( 160.0f - 4.0f * .5f,sliderheight - 48.0f , 4.0f, 48.0f);
     centerView.userInteractionEnabled = NO;
     
     [self addSubview:centerView];
---
+++ Lib/IDPDegreePickerLowBaseView.h
@@ -0,0 +1,13 @@
+//
+//  IDPDegreePickerLowBaseView.h
+//  IDPDegreePickerExample
+//
+//  Created by 能登 要 on 2014/04/04.
+//  Copyright (c) 2014年 Irimasu Densan Planning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDPDegreePickerLowBaseView : UIView
+
+@end
